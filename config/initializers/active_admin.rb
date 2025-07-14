e188cbb386555b2e59f1219b7db00526e5e522ea
MrGr33n98 <felipehhenriquee@gmail.com>
2025-07-14 10:03:58 -0300

    chore: update ActiveAdmin site title to Compare Solar Dashboard

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,7 +6,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Rails Blog Demo"
+  config.site_title = "Compare Solar Dashboard"
 
   config.namespace :admin do |admin|
     admin.build_menu do |menu|
